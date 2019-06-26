085e766442492b62d78f3cc56620d179761069f4
Kimberly Rouse <ksrouse94@gmail.com>
2019-06-26 23:15:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,10 +12,10 @@ class Anagram
   
   def match(anagrams)
     #create empty array to hold matching result(s)
+    result = []
     #iterate over array of possible anagrams
     anagrams.each do |word|
       if @entry.split("") == word
-        result = []
         result << word
       else
         result = []
