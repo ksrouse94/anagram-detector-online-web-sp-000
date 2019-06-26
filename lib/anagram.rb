40ace9eff284de4af15b94d48184d417d3b1cc30
Kimberly Rouse <ksrouse94@gmail.com>
2019-06-26 22:49:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  
+  anagrams = []
+  
+  #setter and getter for initialized word 
+  attr_accessor :entry
+  
+  def initialize(entry)
+    @entry = entry 
+  end  
+  
+  def match(anagrams)
+    result = []
+    anagrams.each do |word|
+      if @entry.split("").sort == word
+        result << word
+        result
+      end  
+    end  
+  end
+end
